c46254906e98a30f21e8ce412dc8c6f22e292f69
Daniel Lough <daniel.lough@lego.com>
2019-09-02 11:48:17 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Layout from "./layout";
+import Split from "./Split";
+import Theme from "./theme";
+import webpackRules from "./webpackRules";
+
+export { Layout, Split, webpackRules };
+
+export default Theme;
